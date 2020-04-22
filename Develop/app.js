5826afadcd74bc664e15d94b82ae1aa7e9b09a3f
Andrew Thomas <athomik79@gmail.com>
2020-04-22 15:02:10 -0400

    delete function working. Creates unique ids for each note and filters through array

--- Develop/app.js
+++ Develop/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get("/", function(req, res){
     res.sendFile(path.join(mainDir, "index.html"));
-})
+});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(mainDir, "notes.html"));
@@ -20,12 +20,34 @@ app.get("/notes", function(req, res) {
 
 app.get("/api/notes", function(req, res){
     res.sendFile(path.join(__dirname, "/db/db.json"));
-})
+});
 
 app.post("/api/notes", function(req, res){
     let userNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
+    let noteID = (userNotes.length).toString();
+    newNote.id = noteID;
     userNotes.push(newNote);
+    // userNotes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(userNotes));
+    console.log("Note saved to db", newNote);
+    res.json(userNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res){
+    let userNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note ID ${noteID}`);
+    userNotes = userNotes.filter(currNote => {
+        return currNote.id !=noteID;
+    })
+
+    for (currNote of userNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
     fs.writeFileSync("./db/db.json", JSON.stringify(userNotes));
     res.json(userNotes);
 })
