a14653c7d090f7cf2a1e83556d38f30bb4d91e2d
Andrew Thomas <athomik79@gmail.com>
2020-04-22 15:27:12 -0400

    adding new ids based on position in array when adding or deleting notes

--- Develop/app.js
+++ Develop/app.js
@@ -10,17 +10,22 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(mainDir, "index.html"));
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(mainDir, "notes.html"));
 });
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(mainDir, "notes.html"));
-})
-
-app.get("/api/notes", function(req, res){
+app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+app.get("/api/notes:id", function(req, res){
+    let userNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(userNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req,res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+})
 
 app.post("/api/notes", function(req, res){
     let userNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -38,7 +43,7 @@ app.delete("/api/notes/:id", function(req, res){
     let userNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteID = req.params.id;
     let newID = 0;
-    console.log(`Deleting note ID ${noteID}`);
+    console.log(`Deleting note # ${noteID}`);
     userNotes = userNotes.filter(currNote => {
         return currNote.id !=noteID;
     })
